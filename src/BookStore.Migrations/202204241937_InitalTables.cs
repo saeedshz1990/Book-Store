e1fc39a77b9009b90e35c104f963590a01b5e03d
Saeed Ansari <68070998+saeedshz1990@users.noreply.github.com>
2022-04-24 19:41:22 +0430

    Create Layer Migrations

---
+++ src/BookStore.Migrations/202204241937_InitalTables.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator;
+
+namespace BookStore.Migrations
+{
+    [Migration(202204241937)]
+    public class _202204241937_InitalTables : Migration
+    {
+        public override void Up()
+        {
+            CreateCategory();
+            CreateBooks();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Books");
+            Delete.Table("Categories");
+        }
+
+        private void CreateBooks()
+        {
+            Create.Table("Books")
+                            .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                            .WithColumn("Title").AsString(50).NotNullable()
+                            .WithColumn("Pages").AsInt32().NotNullable()
+                            .WithColumn("Description").AsString(500).Nullable()
+                            .WithColumn("Author").AsString(100).NotNullable()
+                            .WithColumn("CategoryId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Books_Categories", "Categories", "Id")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreateCategory()
+        {
+            Create.Table("Categories")
+                            .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                            .WithColumn("Title").AsString(50).NotNullable();
+        }
+
+    }
+}
